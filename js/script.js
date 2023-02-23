c69c1993f40d8f1d7947eb5f91d869dbb6327eaa
Christy Fury <christy.fury@gmail.com>
2023-02-22 21:52:13 -0500

    creates background images for living room and study

--- js/script.js
+++ js/script.js
@@ -1,4 +1,52 @@
- /*----- constants -----*/
+// create an object for each room
+
+const livingRoom {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// creates game play object
+// class GamePlay {
+//     constructor(name)
+//     {
+//     this.name = name;
+//     this.inventory = [];
+//     }
+
+//     // methods for gameplay
+
+
+// }
+
+// const player = new GamePlay("name");
+
+
+
+
+
+
+
+
+
+/*----- constants -----*/
 
 
   /*----- state variables -----*/
