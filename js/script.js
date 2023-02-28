65c6d65946966e070984be91fe363c18516a5888
Christy Fury <christy.fury@gmail.com>
2023-02-27 20:26:54 -0500

    added click events to atrium key and door

--- js/script.js
+++ js/script.js
@@ -1,20 +1,90 @@
+"use strict";
 
+const inventoryItems = [];
+const $inventoryBar = $(".inventory-bar")
+const $inventoryItems = $("#inventory-items")
+const $inventoryItem = $("#inventory-items li")
 
-$(".door").click(function(){
-  $("#living-room").hide();
+//hide welcome screen on click of enter button
+$("#enterButton").on("click", function(evt){
+  console.log("button clicked");
+  $("#welcome-screen").addClass("hide");
+  gameStart();
 })
 
-const gamePlay = newGame ({
-  state: "login", 
-  transitions:{
-    room1: {},
-    room2: {},
+// start new game with 
+function gameStart(){
+  console.log('New game started');
+  //new NewGame();  
+}
+
+$(".atrium-key").click(function(evt){
+  console.log("clicked key");
+  inventoryItems.push("key");
+  $inventoryItems.append("<li ><img src = 'https://img.icons8.com/ios/256/password1--v1.png'></li>");
+  $(".atrium-key").addClass("hide")
+
+  console.log(inventoryItems)
+})
+
+$(".atrium-door").click(function(evt){
+  console.log("clicked door");
+  if (inventoryItems.includes("key")){
+    console.log("door unclocked");
+    
 
   }
+  else console.log("Hmm seems to be locked")
+});
 
-})
 
-let state = {};
+// $(".atrium-door").on("click", funcion(){
+
+// })
+
+
+
+// create object with each room and contents
+
+const rooms = {
+  atrium: {
+    key: "key",
+    door: "door"
+  },
+  livingRoom: {
+    key: "book",
+    door: "bookshelf"
+  }
+}
+
+
+// $key.click(function(evt){
+//   console.log("clicked key")
+
+// })
+
+class NewGame {
+  constructor()
+  {
+    this.room = room,
+    this.inventory = []
+  }
+  
+  
+}
+
+
+// const gamePlay = newGame ({
+//   state: "login", 
+//   transitions:{
+//     room1: {},
+//     room2: {},
+
+//   }
+
+// })
+
+// let state = {};
 
 // create an object for each room
 
