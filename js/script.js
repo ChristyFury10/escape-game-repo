4c694b77f91f4244ee6341db6a78ac247ddbd50e
Christy Fury <christy.fury@gmail.com>
2023-02-28 15:14:46 -0500

    added images for welcome screen and added key and door for living room

--- js/script.js
+++ js/script.js
@@ -3,38 +3,69 @@
 const inventoryItems = [];
 const $inventoryBar = $(".inventory-bar")
 const $inventoryItems = $("#inventory-items")
-const $inventoryItem = $("#inventory-items li")
+const $inventoryItem = $("li")
+const $currentRoom = $("#room")
+
+let livingRoomBackground = 
 
 //hide welcome screen on click of enter button
 $("#enterButton").on("click", function(evt){
-  console.log("button clicked");
+  console.log("you have entered the house");
   $("#welcome-screen").addClass("hide");
-  gameStart();
+  //gameStart();
 })
 
-// start new game with 
-function gameStart(){
-  console.log('New game started');
-  //new NewGame();  
-}
+// // start new game with 
+// function gameStart(){
+//   console.log('New game started');
+//   //new NewGame();  
+// }
 
+// room change functions
+const toLivingRoom = function(){
+  //hide all atrium elements and show all living room elements
+  $currentRoom.addClass("living-room");
+  console.log("now in living room");
+  }
+  
+  // clicking book"key" inliving room
+  $(".living-room-key").click(function(evt){
+    console.log("you found a book")
+    let $newItem = $("<li>book</li>")
+    $inventoryItems.append($newItem);
+    $newItem.addClass("invItem");
+    $(".living-room-key").addClass("hide")
+  })
+
+
+
+
+// click event for atroum key, hide key on screen and add to inventory box and array, then call fucntion to start next room
 $(".atrium-key").click(function(evt){
   console.log("clicked key");
   inventoryItems.push("key");
-  $inventoryItems.append("<li ><img src = 'https://img.icons8.com/ios/256/password1--v1.png'></li>");
-  $(".atrium-key").addClass("hide")
+  let $newItem = $("<li>key</li>")
+  $inventoryItems.append($newItem);
+  $newItem.addClass("invItem");
+  $(".atrium-key").addClass("hide");
+  
 
   console.log(inventoryItems)
 })
 
+//door locked unless key is in inventory array
 $(".atrium-door").click(function(evt){
   console.log("clicked door");
   if (inventoryItems.includes("key")){
     console.log("door unclocked");
-    
+    $(".atrium-door").addClass("hide");
+    toLivingRoom();
 
   }
-  else console.log("Hmm seems to be locked")
+  else {
+    console.log("Hmm seems to be locked");
+    // add message in screen...
+  }
 });
 
 
@@ -46,16 +77,16 @@ $(".atrium-door").click(function(evt){
 
 // create object with each room and contents
 
-const rooms = {
-  atrium: {
-    key: "key",
-    door: "door"
-  },
-  livingRoom: {
-    key: "book",
-    door: "bookshelf"
-  }
-}
+// const rooms = {
+//   atrium: {
+//     key: "key",
+//     door: "door"
+//   },
+//   livingRoom: {
+//     key: "book",
+//     door: "bookshelf"
+//   }
+// }
 
 
 // $key.click(function(evt){
@@ -63,15 +94,15 @@ const rooms = {
 
 // })
 
-class NewGame {
-  constructor()
-  {
-    this.room = room,
-    this.inventory = []
-  }
+// class NewGame {
+//   constructor()
+//   {
+//     this.room = room,
+//     this.inventory = []
+//   }
   
   
-}
+// }
 
 
 // const gamePlay = newGame ({
