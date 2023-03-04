100478e706e8c7845b31360364be6b80088d934d
Christy Fury <christy.fury@gmail.com>
2023-03-04 10:00:42 -0500

    MVP met

--- js/script.js
+++ js/script.js
@@ -10,9 +10,12 @@ const $atriumKey = $(".key")
 const $atriumDoor = $(".exit-door")
 const $livingRoomKey = $(".living-room-key")
 const $livingRoomDoor = $(".living-room-door")
+const $messaegLine = $("#goal")
+const $atriumLight = $(".atrium-light")
+const $livingRoomFalseDoor = $(".living-room-false-door")
 
 
-let livingRoomBackground = 
+
 
 //hide welcome screen on click of enter button
 $("#enterButton").on("click", function(evt){
@@ -20,6 +23,7 @@ $("#enterButton").on("click", function(evt){
   // $("#welcome-screen").addClass("hide");
   $("#welcome-screen").css("display", "none")
   toAtrium();
+  $messaegLine.text("You entered the house and the front door vanished behing you... now what?") 
 
   //gameStart();
 })
@@ -30,15 +34,20 @@ const toAtrium = function() {
   $("#room").removeClass("front-of-house").addClass("atrium");
   $atriumKey.removeClass("hide").addClass("atrium-key");
   $atriumDoor.removeClass("hide").addClass("atrium-door");
+  $inventoryBar.removeClass("hide");
+  $atriumLight.removeClass("hide")
+
 }
 
 const toLivingRoom = function(){
   //hide all atrium elements and show all living room elements
   $currentRoom.addClass("living-room");
   console.log("now in living room");
+  $messaegLine.text("What a surprise, another room!")
   $("#room").removeClass("atrium").addClass("living-room");
   $livingRoomKey.removeClass("hide");
   $livingRoomDoor.removeClass("hide");
+  $livingRoomFalseDoor.removeClass("hide")
   }
 
   var modal = document.getElementById("success");
@@ -46,6 +55,13 @@ const toLivingRoom = function(){
     modal.style.display = "block";
   }
   
+// open inventory bar on click
+$inventoryBar.click(function(evt){
+  console.log("clicked inventory");
+  $inventoryBar.toggleClass("small");
+
+});
+
 // click event for atrium key, hide key on screen and add to inventory box and array, then call fucntion to start next room
 $atriumKey.on("click", function(evt){
   console.log("clicked key");
@@ -54,7 +70,8 @@ $atriumKey.on("click", function(evt){
   $inventoryItems.append($newItem);
   $newItem.addClass("invItem");
   $atriumKey.addClass("hide");
-  console.log(inventoryItems)
+  console.log(inventoryItems);
+  $messaegLine.text("You found a key behind the curtain!")
 })
 
 //door locked unless key is in inventory array
@@ -67,13 +84,24 @@ $atriumDoor.click(function(evt){
   }
   else {
     console.log("Hmm seems to be locked");
+    $messaegLine.text("Hmm this door seems to be locked")
     // add message in screen...
   }
 });
 
+$atriumLight.click(function(){
+  console.log("clicked light");
+  if (inventoryItems.includes("key")){
+    console.log("electrocuted");
+    toGameOver();
+  }
+
+})
+
   // clicking book"key" inliving room
   $livingRoomKey.click(function(evt){
-    console.log("you found a book")
+    console.log("you found a book");
+    $messaegLine.text("You found a book under the couch!")
     inventoryItems.push("book");
     let $newItem = $("<li>book</li>")
     $inventoryItems.append($newItem);
@@ -88,8 +116,13 @@ $atriumDoor.click(function(evt){
       success();
       }
     else {
-      console.log("this bookshelf sems to be missing something")
+      console.log("this bookshelf sems to be missing something");
+      $messaegLine.text("This bookshelf looks likt it might be missing something")
     }
+  })
+
+  $livingRoomFalseDoor.click(function(){
+    console.log("clicked false door")
   })
 
 
